66c8995edd37f201e1e1e681ebe1270f41b3d438
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-09-14 06:22:45 -0700

    print_most_numbers.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,16 +1,13 @@
-#include "main.h"
-/**
- * main - prints _putchar, followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char str[] = "_putchar";
-int ch;
-for (ch = 0; ch < 8; ch++)
-_putchar(str[ch]);
-_putchar('\n');
+#include <unistd.h>
 
-return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
