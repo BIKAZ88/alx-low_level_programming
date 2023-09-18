00c4beb87eec9ce56375d767e252d56ab0a31343
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-09-18 07:50:12 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings.
- */
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
