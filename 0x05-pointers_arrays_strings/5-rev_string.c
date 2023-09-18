af081243b5f721d3e81dea1fb655e827ce62140b
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-09-18 09:51:23 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
 /**
- *rev_string - reverses a string.
- *@s : string to be reserved.
- *Return void
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
-void rev_string(char *s);
+void rev_string(char *s)
 {
-int l, i;
-char temp;
-for (l = 0; s[l] != '\0'; ++l)
-;
-for (i =0; i < l / 2; i++)
-{
-temp = s[i];
-s[i] = s[l - 1 - i];
-s[l - 1 - i] = temp;
-}
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
