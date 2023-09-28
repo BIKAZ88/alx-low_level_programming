a29eac2e6599702634ce3841dfbf282883bb2db7
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-09-28 08:17:24 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,15 +8,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-int pw = 1;
-
-if (y < 0)
-return (-1);
-
-if (!y)
-return (1);
-
-pw = _pow_recursion(x, y - 1);
-
-return (pw * x);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
