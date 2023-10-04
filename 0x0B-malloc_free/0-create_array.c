ffc442b8336bab4007217ab7ed4f459d4e424924
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-10-04 06:01:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *n = malloc(size);
+char *array = NULL;
+	unsigned int i;
 
-if (size == 0 || n == 0)
-return (0);
-
-while (size--)
-n[size] = c;
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
+}
 }
