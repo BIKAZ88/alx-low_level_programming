f1eabe77174fa3acf1d673a400170d1acfb06a79
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-10-10 05:18:44 -0700

    02-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - print name of this source file
+ * main - entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-printf("%s\n", __FILE__);
- 
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
