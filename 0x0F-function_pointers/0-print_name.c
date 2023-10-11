7847fa06989348a84b5b00c4a62b12f1d325b4ef
BIKAZ88 <moh.yousif.moh@gmail.com>
2023-10-10 23:59:26 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name
+ * @name: name of the person
+ * @f: the printing function pointer
+ * 
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char*))
+{
+if (name && f)
+f(name);
+}
